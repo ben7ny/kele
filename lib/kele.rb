f7aefa682d7a5bdafddffe7d09da6e849351ae34
Ben Jason <benjason777@gmail.com>
2018-08-26 17:50:08 -0400

    add ability to authenticate with Bloc's API

---
+++ lib/kele.rb
@@ -0,0 +1,25 @@
+class Kele
+  require 'httparty'
+  include HTTParty
+
+  def initialize(email, password)
+    @bloc_base_api_url = "https://www.bloc.io/api/v1"
+
+    options = {
+      body: {
+        email:    email,
+        password: password
+      }
+    }
+    response = self.class.post("#{@bloc_base_api_url}/sessions", options)
+    if response["auth_token"]
+      @auth_token = response["auth_token"]
+    else
+      raise response["message"]
+    end
+  end
+
+end
+
+
+
