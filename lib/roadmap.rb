6e26e41e895b1769de98d8212d95f9d582c558e1
Ben Jason <benjason777@gmail.com>
2018-08-27 22:30:58 -0400

    add method to retrieve remaining checkpoints

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -15,6 +15,14 @@ module Roadmap
     JSON.parse(response.body)
   end
 
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get(
+      "#{@bloc_base_api_url}/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section",
+      headers: { "authorization" => @auth_token }
+    )
+    JSON.parse(response.body)
+  end
+
 end
 
 
